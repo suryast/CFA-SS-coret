228bd2d24133e3ad1b6b47afc8a7a53614017f8c
Surya Setiyaputra <hello@setiyaputra.me>
2017-04-25 15:55:49 +1000

    implement payment flow correctly, bug: cannot reset cart to zsh after payment completed

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,12 +3,13 @@
 class ChargesController < ApplicationController
   before_action :amount_to_be_charged
   before_action :set_description
+  before_action :set_email
 
   def new
   end
 
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail],
+    customer = StripeTool.create_customer(email: @email,
                                           stripe_token: params[:stripeToken])
 
     charge = StripeTool.create_charge(customer_id: customer.id,
@@ -16,6 +17,7 @@ class ChargesController < ApplicationController
                                       description: @description)
 
     redirect_to thanks_path
+
     rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
@@ -24,12 +26,16 @@ class ChargesController < ApplicationController
   private
 
     def amount_to_be_charged
-      @amount = current_order.subtotal*100
+      @amount = current_order.subtotal.to_i*100
     end
 
   # as a private method
   def set_description
-    @description = "Some amazing product"
+    @description = "Your Purchase from Coret"
+  end
+
+  def set_email
+    @email = current_user.email
   end
 
   def thanks
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,5 @@
 class OrderItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_order_item, only: [:show, :edit, :update, :destroy]
 
   # GET /order_items
